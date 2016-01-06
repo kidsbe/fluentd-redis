20ce01ecf4a30b9f85095f3464ea24418cd6a6e8
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2016-01-06 20:22:29 +0300

    handy local builder

--- build_local.sh
+++ build_local.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.test.yml build && \
-docker-compose -f docker-compose.test.yml run -e DEBUG_LOOP=$DEBUG_LOOP sut
+if [ -n "$DEBUG" ]; then
+    docker-compose -f docker-compose.test.yml build && \
+    docker-compose -f docker-compose.test.yml run -e DEBUG_LOOP=$DEBUG_LOOP sut
+else
+    time docker run --rm -it \
+        --privileged \
+        --volumes-from builder_cache \
+        -v $(pwd):/app \
+        tutum/builder
+fi;
